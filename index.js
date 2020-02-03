ae5180413390c468a13eef01f0da9010c6bce786
The Devout Programmer <github@devoutprogrammer.com>
2020-02-04 09:53:19 +1100

    refactored

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
 'use strict'
-module.exports = (quantity, item) => {
-    return quantity > 1 ? `${item}s` : `${item}`
-}
+module.exports = (quantity, item) => return quantity > 1 ? `${item}s` : `${item}`
